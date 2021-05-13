7a4118e5d49145a086a348748bcc3c7df2b31b6f
Harshdeep <ozaiworld@gmail.com>
2021-05-13 14:10:30 -0400

    Initial Commit

---
+++ c-ray-tracer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-ray-tracer
+//
+//  Created by Harshdeep Kahlon on 5/13/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
